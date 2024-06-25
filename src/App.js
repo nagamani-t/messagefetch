b591577ff4f3ef5515f839d92d71fb0abb6563dd
nagamani <nagamani@neokred.tech>
2024-06-25 11:07:15 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-
 import './App.css';
-
 
 function App() {
   return (
-    <div style={{height:"100vh",display:"flex",justifyContent:'center',alignItems:"center"}}>
-      <div style={{ display: "flex", flexDirection: 'column', alignItems: "center", gap: "10px" }}><img src={`${process.env.PUBLIC_URL}/dashboard.gif`} alt="Loading GIF" height={120} width={120} /> 
-      welcome to dashboard
-      <p style={{fontSize:'12px'}}>your sms fetched successfully</p></div>
+    <div className="App">
+      <div style={{ display: "flex", flexDirection: 'column', alignItems: "center", gap: "10px" }}>
+        <img src={`${process.env.PUBLIC_URL}/dashboard.gif`} alt="Loading GIF" height={120} width={120} /> 
+        Welcome to dashboard
+        <p style={{ fontSize: '12px' }}>Your SMS fetched successfully</p>
+      </div>
     </div>
   );
 }
