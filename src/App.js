2b7e9e9e153fc118416e762f922c0667fe0a7179
nagamani <nagamani@neokred.tech>
2024-06-25 09:58:23 +0530

    inline css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div style={{height:"100vh",display:"flex",justifyContent:'center',alignItems:"center"}}>
       <div style={{ display: "flex", flexDirection: 'column', alignItems: "center", gap: "10px" }}><img src={`${process.env.PUBLIC_URL}/dashboard.gif`} alt="Loading GIF" height={120} width={120} /> 
       welcome to dashboard
       <p style={{fontSize:'12px'}}>your sms fetched successfully</p></div>
