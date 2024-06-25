1b4038a2d322dc5c284aecd04824b9b0e4b6aff3
nagamani <nagamani@neokred.tech>
2024-06-25 17:43:58 +0530

    params integration

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 
 function App() {
+  const [userData, setUserData] = useState({ name: '', phoneNumber: '', email: '' });
+
+  useEffect(() => {
+    const params = getQueryParams();
+    console.log("params",params)
+    setUserData(params);
+  }, []);
   return (
     <div style={styles.container}>
-    <div style={styles.centeredContent}>
-    <div style={{ display: "flex", flexDirection: 'column', alignItems: "center", }}>
-                <img src={`${process.env.PUBLIC_URL}/dashboard.gif`} alt="Loading GIF" height={120} width={120} />
-                <p style={{fontSize:'24px'}}>Welcome to dashboard!</p>
-                <p style={{ fontSize: '12px' }}>Your SMS fetched successfully</p>
-            </div>
+      <div style={styles.centeredContent}>
+        <div style={{ display: "flex", flexDirection: 'column', alignItems: "center", }}>
+          <img src={`${process.env.PUBLIC_URL}/dashboard.gif`} alt="Loading GIF" height={120} width={120} />
+          <p style={{ fontSize: '24px' }}>Welcome to dashboard {userData?.name}!</p>
+          <p style={{ fontSize: '12px' }}>Your SMS fetched successfully from {userData?.phonenumber} contact number</p>
+        </div>
+      </div>
     </div>
-</div>
   );
 }
 const styles = {
   container: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-   height:'900px',
-      margin: 0,
-      padding: 0,
-   
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '900px',
+    margin: 0,
+    padding: 0,
   },
   centeredContent: {
-      textAlign: 'center',
+    textAlign: 'center',
   },
 };
 
-
+function getQueryParams() {
+  const params = new URLSearchParams(window.location.search);
+  return {
+    name: params.get('name'),
+    phoneNumber: params.get('phoneNumber'),
+    email: params.get('email'),
+  };
+}
 
 export default App;
