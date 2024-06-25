cd52fca01bdb4377db91b6b22bb173ba9640319f
nagamani <nagamani@neokred.tech>
2024-06-25 15:12:27 +0530

    changes

--- src/dashboard.js
+++ src/dashboard.js
@@ -3,7 +3,7 @@ import './App.css';
 
 const Dashboard = () => {
     return (
-        <div style={{ display: "flex ", justifyContent: "center", alignItems: "center",backgroundColor:"red",margin:"100px", }}><div style={{ display: "flex", flexDirection: 'column', alignItems: "center", gap: "10px" }}>
+        <div style={{ display: "flex ", justifyContent: "center", alignItems: "center",backgroundColor:"red",height:"50vh" }}><div style={{ display: "flex", flexDirection: 'column', alignItems: "center", gap: "10px" }}>
             <img src={`${process.env.PUBLIC_URL}/dashboard.gif`} alt="Loading GIF" height={120} width={120} />
             <p >Welcome to dashboard</p>
             <p style={{ fontSize: '12px' }}>Your SMS fetched successfully</p>
